66652fe4f1282b4936b02a9836165be1eb868a16
liujiang <liujianga@998.com>
2024-12-04 15:54:09 +0800

    firstCommit

---
+++ lib/r.dart
@@ -0,0 +1,18 @@
+class R {
+  static const String ic_hotelList_map_png =
+      'assets/images/ic_hotelList_map.png';
+  static const String ic_location_expand_png =
+      'assets/images/ic_location_expand.png';
+  static const String ic_mine_advise_png = 'assets/images/ic_mine_advise.png';
+  static const String ic_mine_avatar_png = 'assets/images/ic_mine_avatar.png';
+  static const String ic_mine_banner_enterprise_png =
+      'assets/images/ic_mine_banner_enterprise.png';
+  static const String ic_mine_banner_wallet_png =
+      'assets/images/ic_mine_banner_wallet.png';
+  static const String ic_mine_comment_png = 'assets/images/ic_mine_comment.png';
+  static const String ic_mine_identity_png =
+      'assets/images/ic_mine_identity.png';
+  static const String ic_mine_invoice_png = 'assets/images/ic_mine_invoice.png';
+  static const String ic_mine_qrcode_png = 'assets/images/ic_mine_qrcode.png';
+  static const String ic_mine_scan_png = 'assets/images/ic_mine_scan.png';
+}
